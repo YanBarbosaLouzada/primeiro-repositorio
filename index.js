f6a87b31087358c21fc3e53602cec59cd25053fa
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2022-09-16 16:52:18 -0300

    dia 16/09

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ *  tipos primitivos 
+ *  variaveis no javascript
+ *  operadores matematicos do javascript
+ */
+/** (console.log) Serve para ecrever mensagens no terimanal(console) */
+/** string  = Texto  */
+/** number  = número */
+/**boolean verdadeiro ou falso  */
+
+
+console.log('Olá Mundo') 
+console.log(5)
+console.log(42.89)
+console.log(true)
+console.log(false)
+
+/** variáveis */
+console.log('Estudando variáveis')
+/**palavras chaves */
+let /**serve sou uma vex pra declarar a variavel */mensagem = 'Quem estuda progamação é doido'
+console.log(mensagem)
+
+
---
+++ operadores_de_incremento.js
@@ -0,0 +1,30 @@
+//mostra uma mensagem para o usuario 
+alert('ola mundo')
+
+const nome_do_usuario = prompt('Olá, usuário, qual o seu nome?')
+//prompt mostra uma msg e recebe um valor se tiver um const ou let antes do codigo.
+
+//typeof retorna o valor da variavel 
+
+let numeroo = Number(prompt('olá, ' + nome_do_usuario + '. Digite algum numero,por favor '))
+//Number trasnforma em numero
+
+//const sucessor= numeroo + 1
+//const antecessor= numeroo - 1
+//alert('O sucessor de'+ numeroo + ' é ' + sucessor )
+//alert('O antecessor de' + numeroo + ' é ' + antecessor) 
+
+//operaradores de incremento
+
+//const sucessor= numeroo++ //valor da variavel +1
+
+console.log(numeroo++)      //100    // operador de pós-incremento 
+console.log(numeroo)        //101
+console.log(++numeroo)      //102    // pre-incremento
+
+
+//operadores de decremento
+
+console.log(numeroo--)
+console.log(numeroo)
+console.log(--numeroo)
