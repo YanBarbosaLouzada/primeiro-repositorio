cd32a5188343f0b645ed5863d541a9e42ce75674
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2022-09-26 08:58:38 -0300

    Alteração do dia 23/09/2022

---
+++ arrays.js
@@ -0,0 +1,38 @@
+/*Array ou vetores e um tipo especial que te permite armazenar diversos valores em apenas um local */
+/* Criando um array */
+
+
+
+
+
+
+
+//let arr2 = new Array(5)
+//let arr3 = []               //Mais usada
+
+/**
+ * Posições de um array
+ * 
+ * -> A primeira posição de um Array é a posição é 0
+ */
+
+let arr = new Array(10, 15, 9.8, 'Eu sou um texto dentro de um array', 'Olá mundo', true, false, false)
+console.log(arr)
+
+//pega so um valor dentro do array
+
+console.log(arr[4])
+
+// Mudar ou criar um valor da posição sem mecher no texto.
+
+arr[6] = 'outro texto'
+console.log(arr[6])
+console.log(arr)
+
+/*Crie um programa que pergunte quantos produtos um cliente pegou e depois peça para que ele informe
+o preço de cada produto. Ao recuperar possuir o valor de cada produto, faça a soma total da compra e
+informe o maior e o menor preço que foi informado.*/
+
+
+
+
